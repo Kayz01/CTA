e8ef95d59a4c83ccb9502d8b2459f497689dd965
Kayz01 <87199845+Kayz01@users.noreply.github.com>
2022-07-16 13:04:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const { userMention } = require("@discordjs/builders");
 const Discord = require("discord.js");
+const { userInfo } = require("os");
 const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.DIRECT_MESSAGES
+        Discord.Intents.FLAGS.GUILD_MESSAGES
     ]
 });
 require("dotenv").config();
@@ -11,17 +12,36 @@ require("dotenv").config();
 const prefix = "!";
 
 client.on("ready", () => {
-    console.log("Je suis prêt");
+    console.log("Bot Opérationnel");
 });
 
-client.on("message", msg => {
-    if (msg.content === prefix + "pds") {
-        msg.channel.send("pong");
-    }
+client.on("messageCreate", msg => {
 
-    else if (msg.content === prefix + "help"){
-        msg.channel.send("**__Les commandes du bot__**\n - !pds : Prise de service");
+    // Prise de service
+    if (msg.content === prefix + "pds") {
+        const embed = new Discord.MessageEmbed()
+            .setColor ("#33FF33")
+            .setTitle("Prise de Service")
+            .setAuthor("Centre de Traitement d'Alerte")
+            .setDescription("Bonjour, vous venez de prendre votre service, merci de vous mettre dans votre canal respective ! En vous souhaitant une bonne garde.")
+            .setTimestamp();
+            
+        msg.reply({ embeds: [embed]})
     }
+});
+
+client.on("messageCreate", msg => {
+
+    if (msg.content === prefix + "fds") {
+        const embed2 = new Discord.MessageEmbed()
+            .setColor ("#FF0000")
+            .setTitle("Fin de Service")
+            .setAuthor("Centre de Traitement d'Alerte")
+            .setDescription("Bonjour, vous venez de prendre votre fin de service, merci de vous déconnecté de votre canal respective ! Bonne journée / soirée à vous.")
+            .setTimestamp();
+
+        msg.reply({ embeds: [embed2]})
+   }
 });
 
 client.login(process.env.BOT_TOKEN);
